3791321858ec5e73b5cc17f785cd730f00bb3154
Hiroyuki Hagiwara <hagimi@gmail.com>
2014-11-26 17:39:53 +0900

    add vagrant ansible playbook

---
+++ vagrant/provision/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo 'Start ShellScript'
+# タイムゾーンを日本に変更
+sudo cp -p /usr/share/zoneinfo/Japan /etc/localtime
+
+if ! [ `which ansible` ]; then
+    sudo yum update -y
+    sudo yum install -y ansible
+    cp /vagrant/playbook/servers /home/vagrant
+    chmod 644 /home/vagrant/servers
+fi
+
+ansible-playbook -i /home/vagrant/servers /vagrant/ansible.yml
