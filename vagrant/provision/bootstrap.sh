a2c3e000b6f85a4e439dcb42253a0a7ecdb2f47c
Hiroyuki Hagiwara <hagimi@gmail.com>
2014-11-26 17:47:31 +0900

    addinventory file

--- vagrant/provision/bootstrap.sh
+++ vagrant/provision/bootstrap.sh
@@ -7,7 +7,7 @@ sudo cp -p /usr/share/zoneinfo/Japan /etc/localtime
 if ! [ `which ansible` ]; then
     sudo yum update -y
     sudo yum install -y ansible
-    cp /vagrant/playbook/servers /home/vagrant
+    cp /vagrant/servers /home/vagrant
     chmod 644 /home/vagrant/servers
 fi
 
