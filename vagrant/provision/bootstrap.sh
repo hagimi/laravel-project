69f41272bbdd4ed99dd4270115e4d7b38331f8c7
Hiroyuki Hagiwara <hagimi@gmail.com>
2014-11-28 20:01:44 +0900

    php playbook

--- vagrant/provision/bootstrap.sh
+++ vagrant/provision/bootstrap.sh
@@ -7,8 +7,8 @@ sudo cp -p /usr/share/zoneinfo/Japan /etc/localtime
 if ! [ `which ansible` ]; then
     sudo yum update -y
     sudo yum install -y ansible
-    cp /vagrant/servers /home/vagrant
+    cp /vagrant/servers /home/vagrant/
     chmod 644 /home/vagrant/servers
 fi
-
-ansible-playbook -i /home/vagrant/servers /vagrant/ansible.yml
+cp /vagrant/ansible.yml /vagrant/ansible/ansible.yml
+ansible-playbook -i /home/vagrant/servers /vagrant/ansible/ansible.yml
